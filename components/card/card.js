62b72236a3fcdf9e5b937364966c73cb4642bc8f
李彪 <376665329@qq.com>
2020-07-22 10:26:44 +0800

    init

--- components/card/card.js
+++ components/card/card.js
@@ -4,18 +4,22 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    title: String,
-    serviceID: String,
-    remark: String,
-    remainder: Number,
-    endTime: String,
-    someInfo: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    card: [
+      {
+        title: '工商执照领取',
+        serviceID: 'HD（）()01',
+        remark: '法人或企业负责人。。。',
+        remainder: 1,
+        endTime: '2020-06-01',
+        someInfo: 'a'
+      }
+    ]
   },
 
   /**
@@ -25,5 +29,9 @@ Component({
     goToSubMain: function(event) {
       console.log(event)
     },
+    getCardInfo: function() {
+      // let that = this
+      // that.setData
+    }
   }
 })
